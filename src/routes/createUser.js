0851da6e5434c72b53f9c19acb7a0171c42428c7
Victor Assis <victor_assis@poli.ufrj.br>
2021-07-29 22:27:04 -0300

    Improving createUser

--- src/routes/createUser.js
+++ src/routes/createUser.js
@@ -1,33 +1,20 @@
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./../../libs/handler-lib";
+import dynamoDb from "./../../libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     Item: {
       // The attributes of the item to be created
       user_id: uuid.v1(), // The id of the author
       name: data.name, // A unique uuid
-      email: data.email,
+      email: data.email, // Parsed from request body
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+});
