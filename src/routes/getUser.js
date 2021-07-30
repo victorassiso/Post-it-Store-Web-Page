f0fa90075a7da8677e2a7dd35811c7462a5505b4
Victor Assis <victor_assis@poli.ufrj.br>
2021-07-29 22:33:41 -0300

    Adding getUser API

---
+++ src/routes/getUser.js
@@ -0,0 +1,20 @@
+import handler from "./../../libs/handler-lib";
+import dynamoDb from "./../../libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // 'Key' defines the partition key and sort key of the item to be retrieved
+    Key: {
+      user_id: event.pathParameters.id, // The id of the author
+    },
+  };
+
+  const result = await dynamoDb.get(params);
+  if (!result.Item) {
+    throw new Error("Item not found.");
+  }
+
+  // Return the retrieved item
+  return result.Item;
+});
