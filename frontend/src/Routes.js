05bfb72d89b65721f4a4bbd915383a70457fcbbb
Victor Assis <victor_assis@poli.ufrj.br>
2021-07-31 10:47:34 -0300

    Creating Login page with AWS Cognito

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
 export default function Routes() {
   return (
     <Switch>
-        <Route exact path="/">
+
+      <Route exact path="/">
         <Home />
-        </Route>
-        {/*Catch all unmatched routes */}
-        <Route>
-            <NotFound />
-        </Route>
+      </Route>
+
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      
+      <Route>
+        <NotFound />
+      </Route>
+
     </Switch>
   );
 }
