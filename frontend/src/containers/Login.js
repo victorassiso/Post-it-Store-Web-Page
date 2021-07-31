6bedbf86a0ded957fc386e15a3e07ffebc0a21d2
Victor Assis <victor_assis@poli.ufrj.br>
2021-07-31 11:09:06 -0300

    Setting the Login State

--- frontend/src/containers/Login.js
+++ frontend/src/containers/Login.js
@@ -3,9 +3,13 @@ import { Auth } from "aws-amplify";
 
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+
 import "./Login.css";
 
+import { useAppContext } from "../libs/contextLib";
+
 export default function Login() {
+  const { userHasAuthenticated } = useAppContext();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -18,7 +22,7 @@ export default function Login() {
   
     try {
       await Auth.signIn(email, password);
-      alert("Logged in");
+      userHasAuthenticated(true);
     } catch (e) {
       alert(e.message);
     }
