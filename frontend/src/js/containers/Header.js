87e7ce32f5cb77f713f7b4cef3db5f2157fea331
Victor Assis <victor_assis@poli.ufrj.br>
2021-08-01 01:23:55 -0300

    Shopping Cart Scratch

---
+++ frontend/src/js/containers/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Header(props) {
+    return (
+        <header className="row block center">
+            <div>
+                <a href="#/">
+                    <h1>Small Shopping Cart</h1>
+                </a>
+            </div>
+            <div>
+                <a href="#/cart">
+                    Cart {' '}
+                    {countCartItems? (
+                        <button className="badge">{countCartItems}</button>
+                    ) : ('')}
+                </a>
+            </div>
+        </header>
+    );
+}
---
+++ frontend/src/js/containers/data.js
@@ -0,0 +1,24 @@
+const data = {
+    products: [
+        {
+            product_id: '1',
+            name: 'Macbook',
+            price: 1400,
+            image: 'https://picsum.photos/id/180/2400/1600',
+        },
+        {
+            product_id: '2',
+            name: 'Old Car',
+            price: 2400,
+            image: 'https://picsum.photos/id/180/2400/265',
+        },
+        {
+            product_id: '3',
+            name: 'W Shoes',
+            price: 1000,
+            image: 'https://picsum.photos/id/21/3008/2008',
+        },
+    ]
+}
+
+export default data;
